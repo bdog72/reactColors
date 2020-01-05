fb55ca6994f71b25eb8c292e0392566b4a385e2d
Brian Bycynski <high5bri@hotmail.com>
2020-01-05 01:39:32 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//
+//
+// import React from 'react';
+// import Palette from './Palette';
+// import seedColors from './seedColors';
+// import { generatePalette } from './colorHelpers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
+
+export default class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+    return (
+      <div>
+        <Palette {...seedColors[2]} />
+      </div>
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   console.log(generatePalette(seedColors[4]));
+//   return (
+//     <div>
+//       <Palette {...seedColors[2]} />
+//       {/* <Palette palette={seedColors[4]} /> */}
+//     </div>
+//   );
+// }
+// function App() {
+//   console.log(generatePalette(seedColors[4]));
+//   return (
+//     <div>
+//       <Palette {...seedColors[2]} />
+//       {/* <Palette palette={seedColors[4]} /> */}
+//     </div>
+//   );
+// }
+
+// export default App;
