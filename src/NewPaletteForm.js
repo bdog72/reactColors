3c8b6278fb523aee4cd50a363590476276310eec
Brian Bycynski <high5bri@hotmail.com>
2020-01-18 04:38:21 -0500

    finished lecture 213

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -92,6 +92,7 @@ class NewPaletteForm extends Component {
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -120,6 +121,17 @@ class NewPaletteForm extends Component {
     });
   }
 
+  handleSubmit() {
+    let newName = 'Bozo Palette';
+    const newPalette = {
+      paletteName: newName,
+      id: newName.toLowerCase().replace(/ /g, '-'),
+      colors: this.state.colors
+    };
+    this.props.savePalette(newPalette);
+    this.props.history.push('/');
+  }
+
   addNewColor() {
     const newColor = {
       color: this.state.currentColor,
@@ -144,6 +156,7 @@ class NewPaletteForm extends Component {
         <CssBaseline />
         <AppBar
           position="fixed"
+          color="default"
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open
           })}
@@ -160,6 +173,13 @@ class NewPaletteForm extends Component {
             <Typography variant="h6" color="inherit" noWrap>
               Persistent drawer
             </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+            >
+              Save Palette
+            </Button>
           </Toolbar>
         </AppBar>
         <Drawer
