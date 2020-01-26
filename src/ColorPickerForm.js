981f13c7acd93dc634249f733a9916a7055e5457
Brian Bycynski <high5bri@hotmail.com>
2020-01-26 06:16:41 -0500

    finished section 29

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -1,23 +1,42 @@
-import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import { ChromePicker } from "react-color";
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { ChromePicker } from 'react-color';
+
+const styles = {
+  picker: {
+    width: '100% !important',
+    marginTop: '2rem'
+  },
+  addColor: {
+    width: '100%',
+    padding: '1rem',
+    marginTop: '1rem',
+    fontSize: '2rem'
+    // color: 'red'
+  },
+  colorNameInput: {
+    width: '100%',
+    height: '70px'
+  }
+};
 
 class ColorPickerForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentColor: "teal", newColorName: "" };
+    this.state = { currentColor: 'teal', newColorName: '' };
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
-    ValidatorForm.addValidationRule("isColorNameUnique", value =>
+    ValidatorForm.addValidationRule('isColorNameUnique', value =>
       this.props.colors.every(
         ({ name }) => name.toLowerCase() !== value.toLowerCase()
       )
     );
-    ValidatorForm.addValidationRule("isColorUnique", value =>
+    ValidatorForm.addValidationRule('isColorUnique', value =>
       this.props.colors.every(({ color }) => color !== this.state.currentColor)
     );
   }
@@ -35,44 +54,50 @@ class ColorPickerForm extends Component {
       name: this.state.newColorName
     };
     this.props.addNewColor(newColor);
-    this.setState({ newColorName: "" });
+    this.setState({ newColorName: '' });
   }
 
   render() {
-    const { paletteIsFull } = this.props;
+    const { paletteIsFull, classes } = this.props;
     const { currentColor, newColorName } = this.state;
     return (
       <div>
         <ChromePicker
           color={currentColor}
           onChangeComplete={this.updateCurrentColor}
+          className={classes.picker}
         />
-        <ValidatorForm onSubmit={this.handleSubmit} ref='form'>
+        <ValidatorForm onSubmit={this.handleSubmit} ref="form">
           <TextValidator
             value={newColorName}
-            name='newColorName'
+            name="newColorName"
+            variant="filled"
+            margin="normal"
+            placeholder="Color Name"
             onChange={this.handleChange}
-            validators={["required", "isColorNameUnique", "isColorUnique"]}
+            className={classes.colorNameInput}
+            validators={['required', 'isColorNameUnique', 'isColorUnique']}
             errorMessages={[
-              "Enter a color name",
-              "Color name must be unique",
-              "Color already used!"
+              'Enter a color name',
+              'Color name must be unique',
+              'Color already used!'
             ]}
           />
           <Button
-            variant='contained'
-            type='submit'
-            color='primary'
+            variant="contained"
+            type="submit"
+            color="primary"
             disabled={paletteIsFull}
+            className={classes.addColor}
             style={{
-              backgroundColor: paletteIsFull ? "grey" : currentColor
+              backgroundColor: paletteIsFull ? 'grey' : currentColor
             }}
           >
-            {paletteIsFull ? "Palette Full" : "Add Color"}
+            {paletteIsFull ? 'Palette Full' : 'Add Color'}
           </Button>
         </ValidatorForm>
       </div>
     );
   }
 }
-export default ColorPickerForm;
+export default withStyles(styles)(ColorPickerForm);
