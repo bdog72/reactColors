8119e93b75d6c0a93fbf88081bc036327e7e7c54
Brian Bycynski <high5bri@hotmail.com>
2020-01-29 04:47:46 -0500

    added emoji picker

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -11,6 +11,8 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 export default class PaletteMetaForm extends Component {
   // state = {
@@ -50,11 +52,11 @@ export default class PaletteMetaForm extends Component {
 
   render() {
     const { newPaletteName } = this.state;
-
+    const { hideForm } = this.props;
     return (
       <Dialog
         open={this.state.open}
-        onClose={this.handleClose}
+        onClose={hideForm}
         aria-labelledby="form-dialog-title"
       >
         <DialogTitle id="form-dialog-title">Choose a palette name</DialogTitle>
@@ -64,6 +66,7 @@ export default class PaletteMetaForm extends Component {
               Please enter a name for your new beautiful palette. Make sure its
               unique
             </DialogContentText>
+            <Picker />
             <TextValidator
               label="Palette Name"
               value={newPaletteName}
@@ -76,7 +79,7 @@ export default class PaletteMetaForm extends Component {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
+            <Button onClick={hideForm} color="primary">
               Cancel
             </Button>
             <Button variant="contained" color="primary" type="submit">
